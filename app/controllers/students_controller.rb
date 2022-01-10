70dd7befd89690eaf0898712a66369b16d71ead7
duneSpice97 <89211252+duneSpice97@users.noreply.github.com>
2022-01-10 16:19:59 -0700

    commit tree 10989af476b85a761039965b27020bd24b3ec2c2

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,4 +5,12 @@ class StudentsController < ApplicationController
     render json: students
   end
 
+  def by_id
+    student = Student.find_by_id(params[:id])
+    if student
+      render json: student
+    else
+      render json: { error: "Student not found"}, status: :not_found
+    end
+  end
 end
